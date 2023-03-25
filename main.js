babaa883468a79cf3aef393d38104336ce633068
Armandocres <armandocres99@gmail.com>
2023-03-24 22:04:03 -0600

    terminando menu

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const navBarMenu = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.inactive');
+const menuHamburguer = document.querySelector('.menu');
+const menuHamburguerMObile = document.querySelector('.mobile-menu');
 
 navBarMenu.addEventListener('click', showMenu);
+menuHamburguer.addEventListener('click', showMenuHamburguer);
+
+function showMenuHamburguer() {
+  menuHamburguerMObile.classList.toggle('inactive');
+}
 
 function showMenu() {
   menuDesktop.classList.toggle('inactive');
